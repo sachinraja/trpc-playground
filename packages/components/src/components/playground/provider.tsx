1f0f3f4610596ad24b7a0df2ff352707f8607311
Sachin Raja <sachinraja2349@gmail.com>
2022-01-02 12:57:35 -0800

    fix: type error for initialValues

--- packages/components/src/components/playground/provider.tsx
+++ packages/components/src/components/playground/provider.tsx
@@ -1,7 +1,7 @@
 import { HtmlValidTrpcPlaygroundConfig } from '@trpc-playground/types'
 import { createReactQueryHooks } from '@trpc/react'
 import { AnyRouter } from '@trpc/server'
-import { atom } from 'jotai'
+import { Atom, atom } from 'jotai'
 import { Provider as JotaiProvider } from 'jotai'
 import { ComponentChildren } from 'preact'
 import { useMemo } from 'preact/hooks'
@@ -19,6 +19,19 @@ type TrpcClient = ReturnType<typeof trpc.createClient>
 export const trpcClientAtom = atom<TrpcClient>(null!)
 export const configAtom = atom<HtmlValidTrpcPlaygroundConfig>(null!)
 
+/**
+ * create type safe initialValues
+ * @see https://jotai.org/docs/api/core#type-script
+ */
+const createInitialValues = () => {
+  const initialValues: (readonly [Atom<unknown>, unknown])[] = []
+  const get = () => initialValues
+  const set = <Value,>(anAtom: Atom<Value>, value: Value) => {
+    initialValues.push([anAtom, value])
+  }
+  return { get, set }
+}
+
 export const PlaygroundProvider = ({ config, children }: PlaygroundProviderProps) => {
   const queryClient = useMemo(() => new QueryClient(), [])
   const trpcClient = useMemo(() =>
@@ -26,13 +39,14 @@ export const PlaygroundProvider = ({ config, children }: PlaygroundProviderProps
       url: config.endpoint,
     }), [])
 
+  const { get, set } = createInitialValues()
+  set(trpcClientAtom, trpcClient)
+  set(configAtom, config)
+
   return (
     <div className='trpc-playground'>
       <JotaiProvider
-        initialValues={[
-          [trpcClientAtom, trpcClient],
-          [configAtom, config],
-        ]}
+        initialValues={get()}
       >
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
           <QueryClientProvider client={queryClient}>

8fe5e19ce2037cee5faea6625ffee1f8e18bcab6
Sachin Raja <sachinraja2349@gmail.com>
2022-01-06 16:54:14 -0800

    chore: improve dev, can now preview html without publish

--- packages/html/scripts/build.js
+++ packages/html/scripts/build.js
@@ -5,14 +5,22 @@ import { fileURLToPath } from 'node:url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const packageJsonPath = path.resolve(__dirname, '..', 'package.json')
-const { version } = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'))
 
 const distPath = path.join(__dirname, '..', 'dist')
 const indexHtmlPath = path.join(distPath, 'index.html')
 
 const indexHtml = await fs.readFile(indexHtmlPath, 'utf8')
-const cdnAssetsHref = `//cdn.jsdelivr.net/npm/@trpc-playground/html@${version}/dist/assets`
-const cdnHtml = indexHtml.replace(/\/assets/g, cdnAssetsHref)
+
+let assetsHref
+
+if (process.env.NODE_ENV === 'development') {
+  assetsHref = 'http://localhost:45245/assets'
+} else {
+  const { version } = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'))
+  assetsHref = `//cdn.jsdelivr.net/npm/@trpc-playground/html@${version}/dist/assets`
+}
+
+const cdnHtml = indexHtml.replace(/\/assets/g, assetsHref)
 
 const variableName = 'cdnHtml'
 const cdnHtmlDeclaration = `const ${variableName} = ${JSON.stringify(cdnHtml)}`

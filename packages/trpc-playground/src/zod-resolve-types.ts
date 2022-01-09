01f895a61b4f34d33382d8834f153a7a50386984
Sachin Raja <sachinraja2349@gmail.com>
2022-01-09 05:45:19 -0800

    fix: incorrect resolved types

--- packages/trpc-playground/src/zod-resolve-types.ts
+++ packages/trpc-playground/src/zod-resolve-types.ts
@@ -2,13 +2,26 @@ import { AnyRouter } from '@trpc/server'
 import { ZodAny } from 'zod'
 import { printNode, zodToTs } from 'zod-to-ts'
 
-export const zodResolveTypes = (router: AnyRouter) =>
-  Object.entries(router._def.queries).map(([name, query]) => {
+export const zodResolveTypes = (router: AnyRouter) => {
+  const queryNames: string[] = []
+  const queryTypes = Object.entries(router._def.queries).map(([name, query]) => {
+    const stringName = `'${name}'`
+    queryNames.push(stringName)
+
     // @ts-expect-error query type is not defined
     const { node } = zodToTs(query.inputParser as ZodAny)
 
     const inputType = printNode(node)
-
-    const queryType = `declare function query(name: '${name}', input: ${inputType}): void`
+    const queryType = `QueryName extends ${stringName} ? ${inputType}`
     return queryType
   })
+
+  const joinedQueryNames = queryNames.join(' | ')
+  const joinedQueryTypes = `${queryTypes.join(' : ')} : never`
+
+  queryTypes.push(
+    `declare function query<QueryName extends ${joinedQueryNames}>(name: QueryName, input: ${joinedQueryTypes}): void`,
+  )
+
+  return queryTypes
+}

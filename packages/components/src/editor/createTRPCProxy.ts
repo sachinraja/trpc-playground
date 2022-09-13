0efe363e1983b687a6108372abc65fa239d9378c
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-09-13 21:46:42 +0200

    add trpc proxy for running queries

---
+++ packages/components/src/editor/createTRPCProxy.ts
@@ -0,0 +1,40 @@
+interface ProxyCallbackOptions {
+  path: string[]
+  args: unknown[]
+}
+type ProxyCallback = (opts: ProxyCallbackOptions) => unknown
+
+const clientCallTypeMap: Record<string, string> = {
+  query: 'query',
+  mutate: 'mutation',
+}
+
+export const createTRPCProxy = (client: Record<string, unknown>) => {
+  return createProxy(({ args, path }) => {
+    const clientCallType = path.pop()!
+
+    const procedureType = clientCallTypeMap[clientCallType]
+    if (!procedureType) return
+
+    const fullPath = path.join('.')
+    return (client as any)[procedureType](fullPath, ...args)
+  })
+}
+
+const createProxy = (cb: ProxyCallback, ...path: string[]) => {
+  const proxy: unknown = new Proxy(
+    () => {},
+    {
+      get(_target, name) {
+        if (typeof name === 'string') {
+          return createProxy(cb, ...path, name)
+        }
+      },
+      apply(_1, _2, args) {
+        cb({ args, path })
+      },
+    },
+  )
+
+  return proxy
+}

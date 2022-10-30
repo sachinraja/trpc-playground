58279bb4b9a19157e022e53b6b5df32b201793f2
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-10-30 20:18:25 +0100

    print all nested objects in response object

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,8 @@
 import inspect from 'object-inspect'
 import { transform } from 'sucrase-browser'
-export const printObject = (obj: unknown) => inspect(obj, { indent: 2 })
+
+// print all nested objects by setting depth to 0
+export const printObject = (obj: unknown) => inspect(obj, { indent: 2, depth: 0 })
 
 export const transformTs = (code: string) =>
   transform(code, {
